cedfb6d01c7106f76ae167516eb1390aa700d2ca
Constantine811 <constantine@ixlayer.com>
2019-12-09 16:38:39 +0200

    aded all needed staff

---
+++ main.js
@@ -0,0 +1,45 @@
+var app = new Vue({
+	el: '#app',
+	data: {
+		images: [
+			{
+				id: 1,
+				link: './assets/Ellipse1.png'
+			},
+			{
+				id: 2,
+				link: './assets/Layer1.png'
+			},
+			{
+				id: 3,
+				link: './assets/Layer4.png'
+			},
+			{
+				id: 4,
+				link: './assets/Layer5.png'
+			},
+			{
+				id: 5,
+				link: './assets/Layer6.png'
+			},
+			{
+				id: 6,
+				link: './assets/Layer7.png'
+			},
+			{
+				id: 7,
+				link: './assets/Layer8.png'
+			}
+		]
+	},
+	methods: {
+		nextImage() {
+			const first = this.images.shift();
+			this.images = this.images.concat(first);
+		},
+		prevImage() {
+			const last = this.images.pop();
+			this.images = [last].concat(this.images);
+		}
+	}
+})
